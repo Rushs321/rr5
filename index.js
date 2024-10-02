10dd1117b163c6167ecc541fceedf34ce1102b5d
Rushs321 <fyggh566@tutamail.com>
2024-10-02 23:12:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 'use strict';
 const app = require('express')();
+const processRequest = require('./src/proxy.js'); // Import the named export
+const PORT = process.env.PORT || 8080;
+const keyPath = './cert/privkey.pem';
+const certPath = './cert/fullchain.pem';
 const fastify = require('fastify')({
   http2: true,
   https: {
-    key: fs.readFileSync(path.join(__dirname, '..', 'https', 'fastify.key')),
-    cert: fs.readFileSync(path.join(__dirname, '..', 'https', 'fastify.cert'))
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
   }
 })
-const processRequest = require('./src/proxy.js'); // Import the named export
-const PORT = process.env.PORT || 8080;
+
 
 fastify.get('/', processRequest);
 fastify.listen({host: '0.0.0.0', port: PORT });
