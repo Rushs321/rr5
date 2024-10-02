e28d725588ba692ac91af7f617b61063218329d4
Rushs321 <fyggh566@tutamail.com>
2024-10-02 23:09:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 'use strict';
 const app = require('express')();
-const fastify = require('fastify')();
+const fastify = require('fastify')({
+  http2: true,
+  https: {
+    key: fs.readFileSync(path.join(__dirname, '..', 'https', 'fastify.key')),
+    cert: fs.readFileSync(path.join(__dirname, '..', 'https', 'fastify.cert'))
+  }
+})
 const processRequest = require('./src/proxy.js'); // Import the named export
 const PORT = process.env.PORT || 8080;
 
 fastify.get('/', processRequest);
-//fastify.listen({host: '0.0.0.0', port: PORT });
-module.exports = app;
+fastify.listen({host: '0.0.0.0', port: PORT });
